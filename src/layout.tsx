d97569dcdb15f20201153c267ad51f5350d3be4c
Castelblanco <ejcastelg@gmail.com>
2025-11-04 22:29:29 -0500

    feat: se mejora el layout y se implementa snackbar para el boton de compra, como no hace nada se envia un mensaje en el snackbar que dice: Soon

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,51 @@
+import { Box, IconButton, Snackbar } from '@mui/material';
 import { Header } from './components/header';
 import { Outlet } from 'react-router';
+import { useSnackbar } from 'storages/snackbar';
+import CloseIcon from '@mui/icons-material/Close';
 
 export const Layout = () => {
+    const { message, setVoidMessage } = useSnackbar();
+
+    const handleCloseSnackbar = () => setVoidMessage();
+
     return (
         <>
             <Header />
-            <Outlet />
+            <Box
+                sx={{
+                    marginTop: 10,
+                }}
+            >
+                <Outlet />
+            </Box>
+            <Snackbar
+                open={message !== ''}
+                message={message}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                autoHideDuration={5000}
+                onClose={handleCloseSnackbar}
+                action={
+                    <IconButton
+                        size="small"
+                        color="inherit"
+                        onClick={handleCloseSnackbar}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                }
+                sx={{
+                    mt: 6,
+                    '& .MuiSnackbarContent-root': {
+                        bgcolor: 'success.main',
+                        color: 'white',
+                        boxShadow: 3,
+                    },
+                }}
+            />
         </>
     );
 };
