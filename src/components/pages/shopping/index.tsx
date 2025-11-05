11137a38f5163736a837c0deca82c5da1166b0b7
Castelblanco <ejcastelg@gmail.com>
2025-11-04 22:33:49 -0500

    feat: se implementa carrito de compras, para poder ver los productos, añadir x cantidad por producto, borrarlo del carrito, y ver cuanto da el total de la compra

--- src/components/pages/shopping/index.tsx
+++ src/components/pages/shopping/index.tsx
@@ -1,3 +1,60 @@
+import { Box, Typography } from '@mui/material';
+import { ButtonDefault } from 'components/buttons/default';
+import { ButtonLink } from 'components/buttons/link';
+import { ShoppingProduct } from 'components/cards/ShoppingProduct';
+import { ROUTES } from 'constants/routes';
+import { decimalsLimit } from 'helpers/numbers';
+import { useShoppingProducts } from 'storages/shopping_products';
+import { useSnackbar } from 'storages/snackbar';
+
 export default function PageShopping() {
-    return <div>PageShopping</div>;
+    const { products } = useShoppingProducts();
+    const { setMessage } = useSnackbar();
+
+    const totalAmount = decimalsLimit(
+        products.reduce((pre, value) => {
+            return pre + value.price * value.qty;
+        }, 0)
+    );
+
+    const handleCheckout = () => setMessage('Soon');
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: 3,
+                width: '100%',
+                marginBottom: 5,
+            }}
+        >
+            <Box>
+                <Typography>Total Card Amount: ${totalAmount}</Typography>
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                }}
+            >
+                {products.length === 0 ? (
+                    <Typography>There aren't products in the cart.</Typography>
+                ) : (
+                    products.map((product) => (
+                        <ShoppingProduct product={product} key={product.id} />
+                    ))
+                )}
+            </Box>
+            <ButtonLink color="success" to={ROUTES.PRODUCTS}>
+                Continue Shopping
+            </ButtonLink>
+            <ButtonDefault color="success" onClick={handleCheckout}>
+                Checkout
+            </ButtonDefault>
+        </Box>
+    );
 }
