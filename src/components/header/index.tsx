5d93ba66d6369fc3e3a636765b794cec112267e0
Castelblanco <ejcastelg@gmail.com>
2025-11-04 22:32:35 -0500

    feat: se implementa cantidad producto en el carrito

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,10 +9,21 @@ import {
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { APP_TITLE } from '../../constants/app';
 import { useShoppingProducts } from 'storages/shopping_products';
+import { useNavigate } from 'react-router';
+import { ROUTES } from 'constants/routes';
 import logo from 'assets/logo.png';
 
 export const Header = () => {
     const { products } = useShoppingProducts();
+    const navigate = useNavigate();
+
+    const productsCount = products.reduce((pre, value) => {
+        return pre + value.qty;
+    }, 0);
+
+    const handleClickLogo = () => navigate(ROUTES.HOME);
+    const handleGoShopping = () => navigate(ROUTES.SHOPPING);
+
     return (
         <Box>
             <AppBar position="fixed" color="success">
@@ -22,7 +33,14 @@ export const Header = () => {
                         justifyContent: 'space-between',
                     }}
                 >
-                    <Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-around',
+                            alignItems: 'center',
+                            width: '15%',
+                        }}
+                    >
                         <img
                             src={logo}
                             alt="logo"
@@ -30,13 +48,15 @@ export const Header = () => {
                                 width: 40,
                                 height: 40,
                                 borderRadius: '50%',
+                                cursor: 'pointer',
                             }}
+                            onClick={handleClickLogo}
                         />
                         <Typography>{APP_TITLE}</Typography>
                     </Box>
-                    <Typography>Plans</Typography>
-                    <IconButton>
-                        <Badge badgeContent={products.length} color="primary">
+                    <Typography variant="h5">Plans</Typography>
+                    <IconButton color="inherit" onClick={handleGoShopping}>
+                        <Badge badgeContent={productsCount} color="primary">
                             <AddShoppingCartIcon />
                         </Badge>
                     </IconButton>
